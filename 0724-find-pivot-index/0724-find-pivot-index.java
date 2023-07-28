746d8145dbebbcf6d36db24b151d2797c42258aa
Abhi9125 <38842501+Abhi9125@users.noreply.github.com>
2023-07-29 03:53:06 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (15.77%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,19 +1,24 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         
-//         i main tain pivot indx 
-        for(int i = 0; i < nums.length; i++){ 
-            int leftsum = 0 , rightsum = 0;
-//             this for loop use for traversing
-            for(int k = 0; k < nums.length; k++){
-                if(k < i) leftsum += nums[k];
-                else if(k > i) rightsum += nums[k];
-                // System.out.println(leftsum+":"+rightsum);
-            }
-            // System.out.println(i);
-            if(i == 0) leftsum = 0;
-            else if(i == nums.length - 1) rightsum = 0;
-            if(leftsum == rightsum) return i; 
+        int totalsum = 0;
+        
+        for(int val : nums){
+            totalsum += val;
+        }
+        
+        int leftsum = 0;
+        int rightsum = totalsum;
+        
+        for(int i = 0; i < nums.length; i++){
+//             rightsum me curr index ka sum nhi hona chahiye so isliye sabse pahle hm rightsum ko sahi kr rhe hai.
+            rightsum -= nums[i];
+            
+//             Ab leftsum me left ki val h and rightsum m right ki value hai.
+            if(leftsum == rightsum) return i;
+            
+//          
+            leftsum += nums[i];
         }
         return -1;
     }
