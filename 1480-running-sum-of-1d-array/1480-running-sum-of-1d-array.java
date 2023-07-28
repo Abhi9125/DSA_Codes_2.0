cbe9c6c9e3409436e860931e5725b4c8cb63a09d
Abhi9125 <38842501+Abhi9125@users.noreply.github.com>
2023-07-28 19:24:11 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (72.99%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,13 +1,24 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-//         brut froce 
-        int res[]= new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            // int sum = 0;
-            for(int j = 0; j <= i; j++){
-               res[i] += nums[j];
-            }
+
+        
+        //         brut froce 
+    //     int res[]= new int[nums.length];
+    //     for(int i = 0; i < nums.length; i++){
+    //         // int sum = 0;
+    //         for(int j = 0; j <= i; j++){
+    //            res[i] += nums[j];
+    //         }
+    //     }
+    //     return res;
+        
+        
+        
+//         Without extra space and O(n)
+        
+        for(int i = 1; i <  nums.length; i++){
+            nums[i] = nums[i-1] + nums[i];
         }
-        return res;
+        return nums;
     }
 }
