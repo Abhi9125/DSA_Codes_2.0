c8b0ea8f11a04048c87cae918a348f1c1f730c17
Abhi9125 <38842501+Abhi9125@users.noreply.github.com>
2023-08-09 22:45:48 +0530

    Added solution - LeetHub

--- Find Transition Point - GFG/find-transition-point.java
+++ Find Transition Point - GFG/find-transition-point.java
@@ -30,8 +30,8 @@ class GfG {
         while(left <= right){
             int mid = (left + right)/2;
             
-            if(arr[mid] == 0) left = mid + 1;
-            else right = mid - 1;
+            if(arr[mid] == 1) right = mid - 1;
+            else left = mid + 1;
         }
         
         return left;
